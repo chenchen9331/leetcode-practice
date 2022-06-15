481ee82f43d4578fee9a9c9b1211d851a35be11c
chenchen <897642029@qq.com>
2022-06-15 10:01:42 +0800

    commit

---
+++ src/main/java/hot100/第141题环形链表/HasCycle.java
@@ -0,0 +1,110 @@
+package hot100.第141题环形链表;
+
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ *
+ * 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 示例 3：
+ *
+ *
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/linked-list-cycle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class HasCycle {
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(2);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n3;
+
+
+        // Hash表
+        boolean b = getByHash(n1);
+        System.out.println(b);
+
+        // 龟兔赛跑（快慢指针解决）
+        boolean a = getByFS(n1);
+
+    }
+
+    private static boolean getByFS(ListNode n1) {
+        if (n1 == null || n1.next == null) {
+            return false;
+        }
+        ListNode slow = n1;
+        ListNode fast = n1.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+        return true;
+    }
+
+    private static boolean getByHash(ListNode n1) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (n1.next != null) {
+            if (set.contains(n1.next)) {
+                return true;
+            } else {
+                set.add(n1);
+            }
+            n1 = n1.next;
+        }
+        return false;
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
