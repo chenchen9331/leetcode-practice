75234dceb5a74c6165a47ae2e973588db45b8bcc
chenchen <897642029@qq.com>
2022-06-17 12:00:35 +0800

    commit

---
+++ src/main/java/hot100/第543题二叉树的直径/Test.java
@@ -0,0 +1,9 @@
+package hot100.第543题二叉树的直径;
+
+public class Test {
+    public static void main(String[] args) {
+        DiameterOfBinaryTree diameterOfBinaryTree = new DiameterOfBinaryTree();
+        int i = diameterOfBinaryTree.diameterOfBinaryTree(new TreeNode());
+        System.out.println(i);
+    }
+}
